8e59bbfeb3e0c724db18ad42a0aa69f59111d4ff
tarjan-1 <tarjanbawankule1@gmail.com>
2025-11-23 19:32:32 +0530

    implemented resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import dayjs from "dayjs";
 import { navIcons, navLinks } from "#constants";
+import useWindowStore from "#store/window";
 
 const Navbar = () => {
+  const { openWindow } = useWindowStore();
+
   return (
     <nav>
       <div>
@@ -9,8 +12,10 @@ const Navbar = () => {
         <p className="font-bold">Tarjan's Portfolip</p>
 
         <ul>
-          {navLinks.map(({ id, name }) => (
-            <li key={id}>{name}</li>
+          {navLinks.map(({ id, name, type }) => (
+            <li key={id} onClick={() => openWindow(type)}>
+              <p>{name}</p>
+            </li>
           ))}
         </ul>
       </div>

bebffaa850765823ba7e784b701cb03e3dd6ce7a
tarjan-1 <tarjanbawankule1@gmail.com>
2025-11-25 00:23:49 +0530

    implemented home

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import gsap from "gsap";
 import { Draggable } from "gsap/Draggable";
 
-import { Dock, Navbar, Welcome } from "#components";
+import { Dock, Home, Navbar, Welcome } from "#components";
 import {
   Contact,
   Finder,
@@ -29,6 +29,8 @@ const App = () => {
       <Text />
       <Image />
       <Contact />
+
+      <Home />
     </main>
   );
 };
